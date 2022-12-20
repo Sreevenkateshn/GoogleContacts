b49e81f4838cc4b2fac5c42f4183784ac780625e
Sowjanya Reddy <shiny.py21@gmail.com>
2022-12-20 23:13:42 +0530

    gc fcommit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+ReactDOM.render(<App/>,document.getElementById('root'));
+// // DEMO JOKE APP
+// import generateJoke from './generateJoke'
+// import './styles/main.scss'
+// import laughing from './assets/laughing.svg'
+
+// const laughImg = document.getElementById('laughImg')
+// laughImg.src = laughing
+
+// const jokeBtn = document.getElementById('jokeBtn')
+// jokeBtn.addEventListener('click', generateJoke)
+
+// generateJoke()
